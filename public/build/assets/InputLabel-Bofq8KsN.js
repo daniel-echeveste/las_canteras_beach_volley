c53bab9dbd4ef25969d00b053985e8985f72875c
danielecheveste <daniel.echeveste@maresalogistica.com>
2025-12-29 11:46:23 +0000

    build

--- public/build/assets/InputLabel-Bofq8KsN.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-Dh50w0qk.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block font-medium text-sm text-gray-700 "+e,children:t||r})}export{o as I};
